a1580aada4eeb2d9fd9345dca657d5be63e6bf27
syed usman <syed.usman@bontouch.com>
2020-09-27 15:46:37 +0200

    Build compose functions

--- app/src/main/java/com/foodie/composetition/MainActivity.kt
+++ app/src/main/java/com/foodie/composetition/MainActivity.kt
@@ -2,9 +2,32 @@ package com.foodie.composetition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            TestComposable()
+        }
     }
 }
+
+@Preview
+@Composable
+    fun TestComposable() {
+        Surface(color = Color.White) {
+            Text("Test")
+        }
+    }
+
+
